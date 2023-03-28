5a9a1da2197d7ae8a42bbc67b1680dbcd9e172bb
Gsus <js49831@gmail.com>
2023-03-27 19:20:58 -0500

    25. notificaciones via correo electronico con Sendgrid

---
+++ Controllers/NotificacionesController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+using ms_notificaciones.Models;
+
+namespace ms_notificaciones.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class NotificacionesController : ControllerBase
+{
+    [Route("correo")]
+    [HttpPost]
+    public async Task<ActionResult> EnviarCorreo(ModeloCorreo datos)
+    {
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var client = new SendGridClient(apiKey);
+        var from = new EmailAddress("jesus.1701912773@ucaldas.edu.co", "jesus ferney salazar");
+        var subject = datos.asuntoCorreo;
+        var to = new EmailAddress(datos.correoDestino, datos.nombreDestino);
+        var plainTextContent = "text content";
+        var htmlContent = datos.contenidoCorreo;
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        var response = await client.SendEmailAsync(msg);
+        if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+        {
+            return Ok("Correo enviado a la direccion" + datos.correoDestino);
+        }
+        else
+        {
+            return BadRequest("Error enviando el mensaje a la direccion: " + datos.correoDestino);
+        }
+    }
+}
